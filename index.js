b44be7ef2ffcadf2a405283f93a8716d6049de37
yash <yaxj28@gmail.com>
2026-01-11 13:07:59 +0530

    Stage-2: return basic HTTP 200 response

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import net from "node:net";
 
 const server = net.createServer((socket)=>{
-	socket.on("data", (data)=>{
+	socket.on('data', (data) => {
         console.log(data.toString());
-	});
+        const response = "HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n\r\n"
+        const encoding = new TextEncoder().encode(response);
+        socket.write(encoding);
+});
 })
 
 server.listen(3000, () => {
