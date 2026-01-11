6b71f6f3f92963d3850981d5952de46435d3f39c
yash <yaxj28@gmail.com>
2026-01-11 13:11:33 +0530

    Stage-3: parse request line and path

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ import net from "node:net";
 
 const server = net.createServer((socket)=>{
 	socket.on('data', (data) => {
-        console.log(data.toString());
-        const response = "HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n\r\n"
+        const raw = data.toString();
+        const headers  = raw.split("\r\n")[0].split(" ");
+        const method = headers[0];
+        const path = headers[1];
+        console.log("Method: ",method);
+        console.log("Path: ", path);
+        const response = "HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n"
         const encoding = new TextEncoder().encode(response);
         socket.write(encoding);
 });
